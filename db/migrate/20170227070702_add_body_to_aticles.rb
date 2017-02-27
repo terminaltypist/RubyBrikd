aa851bc3e38ab60cad7898d07253dd997ef4c722
terminaltypist <terminaltypist@protonmail.com>
2017-02-26 23:36:30 -0800

    Started on the DB

---
+++ db/migrate/20170227070702_add_body_to_aticles.rb
@@ -0,0 +1,7 @@
+class AddBodyToAticles < ActiveRecord::Migration[5.0]
+  def change
+    add_column :articles, :body, :text
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+  end
+end

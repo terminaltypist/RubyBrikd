1598858817483a4d7fd46456791e8b0aa0e009a1
terminaltypist <terminaltypist@protonmail.com>
2017-02-27 22:23:10 -0800

    Finished Articles input form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,16 @@ class ArticlesController < ApplicationController
     # This variable is a new line in the Articles table
     @article = Article.new
   end
+
+  def create
+    # render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_show(@article)
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end

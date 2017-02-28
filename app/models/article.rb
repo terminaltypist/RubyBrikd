05473a1f33792f83ca19180dbc7aaa3812f4e8d5
terminaltypist <terminaltypist@protonmail.com>
2017-02-27 20:50:46 -0800

    Created Articles DB, model and developed validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-
-
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :body, presence: true, length: {minimum: 50, maximum: 10000}
 end

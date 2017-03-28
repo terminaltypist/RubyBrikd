00693a6732f0ddc0bb3d12d5b311e5542a11037a
terminaltypist <terminaltypist@protonmail.com>
2017-03-27 23:25:43 -0700

    Made the form, the alerts and the Show article more presentable.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save
       # Example of flash message
-      flash[:notice] = "Thank you for your submission"
+      flash[:success] = "Thank you for your submission"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -40,7 +40,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = "Article was deleted"
+    flash[:danger] = "Article was deleted"
     redirect_to articles_path
   end
 

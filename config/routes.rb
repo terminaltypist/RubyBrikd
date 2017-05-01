15a63cc1c3a64388aa6de3f66cb36532affed4ef
terminaltypist <terminaltypist@protonmail.com>
2017-05-01 00:01:19 -0700

    Add Users through UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :comments
   resources :users
+
   root 'articles#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :articles
+
+  get 'signup', to: 'users#new'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,14 +23,6 @@ ActiveRecord::Schema.define(version: 20170501002938) do
     t.integer  "user_id"
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.text     "description"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
